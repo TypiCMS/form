679358dc51575f8abe0c2a63ae01f77ec3245c75
Samuel De Backer <sdebacker@gmail.com>
2018-05-09 21:49:47 +0200

    Name in container set to typicms.form

--- src/TypiCMS/Form/FormServiceProvider.php
+++ src/TypiCMS/Form/FormServiceProvider.php
@@ -19,24 +19,24 @@ class FormServiceProvider extends ServiceProvider
 
     protected function registerErrorStore()
     {
-        $this->app->singleton('adamwathan.form.errorstore', function ($app) {
+        $this->app->singleton('typicms.form.errorstore', function ($app) {
             return new IlluminateErrorStore($app['session.store']);
         });
     }
 
     protected function registerOldInput()
     {
-        $this->app->singleton('adamwathan.form.oldinput', function ($app) {
+        $this->app->singleton('typicms.form.oldinput', function ($app) {
             return new IlluminateOldInputProvider($app['session.store']);
         });
     }
 
     protected function registerFormBuilder()
     {
-        $this->app->singleton('adamwathan.form', function ($app) {
+        $this->app->singleton('typicms.form', function ($app) {
             $formBuilder = new FormBuilder();
-            $formBuilder->setErrorStore($app['adamwathan.form.errorstore']);
-            $formBuilder->setOldInputProvider($app['adamwathan.form.oldinput']);
+            $formBuilder->setErrorStore($app['typicms.form.errorstore']);
+            $formBuilder->setOldInputProvider($app['typicms.form.oldinput']);
             $formBuilder->setToken($app['session.store']->token());
 
             return $formBuilder;
@@ -45,6 +45,6 @@ class FormServiceProvider extends ServiceProvider
 
     public function provides()
     {
-        return ['adamwathan.form'];
+        return ['typicms.form'];
     }
 }
